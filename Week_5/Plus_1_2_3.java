2dd47a67f8071c0067aa38a017bfc6d6aeae24c3
DoHyeon Jeon <whdies816@gmail.com>
2019-05-15 00:28:57 +0900

    Plus 1,2,3 Clear

--- Week_5/Plus_1_2_3.java
+++ Week_5/Plus_1_2_3.java
@@ -1,4 +1,24 @@
 package Week_5;
+// https://www.acmicpc.net/problem/9095
 
-public class Plus_1_2_3 {
+import java.io.*;
+
+public class Plus_1_2_3  {
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int [] answer = new int[12];
+        answer[0] = 1;
+        for(int i = 1; i <= 10; i++) {
+            answer[i] += answer[i - 1];
+            if (i > 1) answer[i] += answer [i - 2];
+            if (i > 2) answer[i] += answer [i - 3];
+        }
+        for(int i = 0 ; i < N; i++) {
+            bw.write(answer[Integer.parseInt(br.readLine())]+ "\n");
+        }
+        br.close();
+        bw.close();
+    }
 }
